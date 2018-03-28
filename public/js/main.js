f295c7c520ef15ef1aa15bcb4016f111e7d307a8
Miles <milesdpratt@gmail.com>
2018-03-28 17:30:12 -0400

    Fix dropdown hover on mobile.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 const alert = document.getElementById("alert");
+const dropdownSmall = document.getElementById("dropdown--small");
+const dropdownBig = document.getElementById("dropdown--big");
 if (alert) {
 	setTimeout(() => {
 		alert.classList.add("alert--hidden");
 	}, 2000);
+}
+
+if (dropdownBig) {
+	dropdownBig.addEventListener("click", ev => {
+		const bigContent = document.getElementById("dropdown__content--big");
+		bigContent.classList.toggle("dropdown__show");
+	});
+}
+
+if (dropdownSmall) {
+	dropdownSmall.addEventListener("click", ev => {
+		const smallContent = document.getElementById("dropdown__content--small");
+		smallContent.classList.toggle("dropdown__show");
+	});
 }
