4319a383b94f6ffb81e6c533cff34d589e3748aa
Miles <milesdpratt@gmail.com>
2018-03-29 11:13:00 -0400

    Wrap UI js in functions. Update styles.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const alert = document.getElementById("alert");
 const dropdownSmall = document.getElementById("dropdown--small");
 const dropdownBig = document.getElementById("dropdown--big");
 const showcase = document.querySelector(".showcase");
+
 if (alert) {
 	setTimeout(() => {
 		alert.classList.add("alert--show");
@@ -11,26 +12,37 @@ if (alert) {
 	}, 3000);
 }
 
-setTimeout(() => {
-	if (showcase) {
-		const height = showcase.firstElementChild.clientHeight;
-		console.log(height);
-		showcase.setAttribute("style", `min-height: ${height + 40}px`);
+function setShowcaseHeight() {
+	setTimeout(() => {
+		if (showcase) {
+			const height = showcase.firstElementChild.clientHeight;
+			console.log(height);
+			showcase.setAttribute("style", `min-height: ${height + 40}px`);
+		}
+	}, 50);
+}
+
+function dropDownEvents() {
+	if (dropdownBig) {
+		dropdownBig.addEventListener("click", ev => {
+			const bigContent = document.getElementById("dropdown__content--big");
+			bigContent.classList.toggle("dropdown__show");
+		});
 	}
-}, 10);
 
-if (dropdownBig) {
-	dropdownBig.addEventListener("click", ev => {
-		const bigContent = document.getElementById("dropdown__content--big");
-		bigContent.classList.toggle("dropdown__show");
-	});
+	if (dropdownSmall) {
+		dropdownSmall.addEventListener("click", ev => {
+			const smallContent = document.getElementById("dropdown__content--small");
+			const dropDownIcon = document.querySelector(".dropdown__icon");
+			smallContent.classList.toggle("dropdown__show");
+			dropDownIcon.classList.toggle("dropdown__icon--clicked");
+		});
+	}
 }
 
-if (dropdownSmall) {
-	dropdownSmall.addEventListener("click", ev => {
-		const smallContent = document.getElementById("dropdown__content--small");
-		const dropDownIcon = document.querySelector(".dropdown__icon");
-		smallContent.classList.toggle("dropdown__show");
-		dropDownIcon.classList.toggle("dropdown__icon--clicked");
-	});
-}
+window.addEventListener("orientationchange", function() {
+	setShowcaseHeight();
+});
+
+setShowcaseHeight();
+dropDownEvents();
